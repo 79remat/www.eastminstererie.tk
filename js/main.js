ece40f5cdc4d36b1c5c0190da4769a49c871f3b0
79remat <59704956+79remat@users.noreply.github.com>
2022-12-24 10:37:38 -0500

    chg

--- js/main.js
+++ js/main.js
@@ -41,15 +41,18 @@ function main() {
 		
 }());
 
-// loader
-  var loader = function() {
-    setTimeout(function() { 
-      if($('#loader').length > 0) {
-       $('#loader').removeClass('show');
-     }
-    }, 1);
-  };
-  loader();
+
 
 }
 main();
+
+
+// loader
+var loader = function() {
+  setTimeout(function() { 
+    if($('#loader').length > 0) {
+     $('#loader').removeClass('show');
+   }
+  }, 1);
+};
+loader();
